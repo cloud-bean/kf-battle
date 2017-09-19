343127e707dd48e37c843b0fa4785f24e1f9b637
hygkui <tom@erealmsoft.com>
2017-09-19 15:54:57 +0800

    Better winPage

---
+++ src/components/winPage/memberTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <table style="width: 100%; border-collapse: collapse; padding: 10px;">
+    <tr>
+      <th>头像</th>
+      <th>名字</th>
+      <th>得分</th>
+      <th>丢分</th>
+      <th>获得经验值</th>
+      <th>贡献值</th>
+    </tr>
+    <tr v-for="member in members" :class="member.isMVP ? 'winner': ''">
+      <td><img  style="width: 60px;"  :src="member.profileImageURL"></td>
+      <td><span>{{member.displayName}}</span></td>
+      <td><span>{{member.get}}</span></td>
+      <td><span>{{member.lost}}</span></td>
+      <td><span>{{ (member.get + member.lost) * 100}}</span></td>
+      <td>
+        <span>{{ parseInt((member.get + member.lost) / groupScore * 100, 10) / 4 }}</span>
+        <Tag v-if="member.isMVP" color="red">MVP</Tag>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+  export default {
+    props: ['members', 'groupScore'],
+  };
+</script>
+
+<style>
+  .winner {
+    background-color: rgba(255, 247, 243, 0.58);
+    margin: 2px;
+  }
+</style>
--- src/pages/winPage.vue
+++ src/pages/winPage.vue
@@ -17,51 +17,11 @@
     </Row>
     <hr>
     <Row style="background: rgba(255,255,255,0.36);">
-      <Col span="12">
-        <table style="width: 100%; text-align: left;">
-          <tr>
-            <th>头像</th>
-            <th>名字</th>
-            <th>得分</th>
-            <th>丢分</th>
-            <th>获得经验值</th>
-            <th>贡献值</th>
-          </tr>
-          <tr v-for="member in getMembers(0)">
-            <td><img style="width: 60px;" :src="member.profileImageURL"></td>
-            <td><span>{{member.displayName}}</span></td>
-            <td><span>{{member.get}}</span></td>
-            <td><span>{{member.lost}}</span></td>
-            <td><span>{{ (member.get + member.lost) * 100}}</span></td>
-            <td>
-              <span>{{ parseInt((member.get + member.lost) / finalScore.left * 100, 10)}}</span>
-              <Tag v-if="member.isMVP" color="red">MVP</Tag>
-            </td>
-          </tr>
-        </table>
+      <Col span="12" style="padding: 20px;">
+        <member-table :members="getMembers(0)" :groupScore="finalScore.left"></member-table>
       </Col>
-      <Col span="12">
-      <table style="width: 100%; text-align: right;">
-        <tr>
-          <th>头像</th>
-          <th>名字</th>
-          <th>得分</th>
-          <th>丢分</th>
-          <th>获得经验值</th>
-          <th>贡献值</th>
-        </tr>
-        <tr v-for="member in getMembers(1)">
-          <td><img  style="width: 60px;"  :src="member.profileImageURL"></td>
-          <td><span>{{member.displayName}}</span></td>
-          <td><span>{{member.get}}</span></td>
-          <td><span>{{member.lost}}</span></td>
-          <td><span>{{ (member.get + member.lost) * 100}}</span></td>
-          <td>
-            <span>{{ parseInt((member.get + member.lost) / finalScore.right * 100, 10)}}</span>
-            <Tag v-if="member.isMVP" color="red">MVP</Tag>
-          </td>
-        </tr>
-      </table>
+      <Col span="12" style="padding: 20px;">
+        <member-table :members="getMembers(1)" :groupScore="finalScore.right"></member-table>
       </Col>
     </Row>
   </div>
@@ -69,6 +29,7 @@
 
 <script>
   import ScoreVS from '../components/battlePage/scoreVS';
+  import MemberTable from '../components/winPage/memberTable';
   import { createNamespacedHelpers } from 'vuex';
   const { mapGetters } = createNamespacedHelpers('battle');
   export default {
@@ -102,6 +63,7 @@
     },
     components: {
       'score-vs': ScoreVS,
+      'member-table': MemberTable,
     },
   };
 </script>
