8b1148c8630632e76af76e662856617571ce7724
hygkui <tom@erealmsoft.com>
2017-09-28 19:03:44 +0800

    font change

--- src/pages/customPage.vue
+++ src/pages/customPage.vue
@@ -1,16 +1,16 @@
 <template>
-  <div style="width: 80%; margin: 0 auto;">
+  <div style="width: 95%; margin: 0 auto;">
     <Row>
       <Col span="8" v-for="team in teamList" :key="team._id" style="padding: 10px;">
         <Card>
           <div @click="selectedTeam = team;">
-            <p>{{team.name}}</p>
+            <p style="font-size: 2em;">{{team.name}}</p>
             <img :src="team.logo.URL" style="width: 120px;">
             <div v-if="selectedTeam && selectedTeam._id == team._id">
-              <Button @click="showLogosModal = true;">更换团队logo</Button>
+              <Button @click="showLogosModal = true;">换战队logo</Button>
               <br>
               <input v-model="team.name">
-              <Button @click="saveTeam(team)">修改团队名字</Button>
+              <Button @click="saveTeam(team)">换个战队名字</Button>
               <hr>
               <div v-for="member in team.students" :key="member._id" style="border: 1px solid rgba(128,128,128,0.26); padding: 5px; margin: 5px; display: inline-block;">
                 <img :src="member.profileImageURL" style="width: 60px;" @click="openSetMemberAvatarModal(member)">
