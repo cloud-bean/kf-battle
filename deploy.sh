08a4ac357cad8912c9937907976ab6710eb8c5a9
hygkui <tom@erealmsoft.com>
2018-10-29 01:20:19 +0800

    update deploy.js

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 npm run build
-rm kf-battle-dist.tar
-tar cf kf-battle-dist.tar dist
+mv dist kf-battle-dist
+tar cf kf-battle-dist.tar kf-battle-dist
 scp kf-battle-dist.tar hygkui@120.25.227.156:/home/hygkui/ 
-rm dist.tar
+rm -rf kf-battle-dist
+rm kf-battle-dist.tar
